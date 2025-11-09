70613ee0351504510c7191031543de89d51909a0
nexus <nexusgood93new@gmail.com>
2025-11-09 16:54:16 +0800

    Initial Commit

---
+++ dice game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dice game
+//
+//  Created by 陳詠平 on 2025/11/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dice game/dice_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  dice_gameApp.swift
+//  dice game
+//
+//  Created by 陳詠平 on 2025/11/9.
+//
+
+import SwiftUI
+
+@main
+struct dice_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
